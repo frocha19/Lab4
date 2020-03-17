58edc413bf62ced8e5634cf47be85651de88e1e4
joelvalenti <valentijoel18@gmail.com>
2020-03-17 14:28:37 -0300

    Navbar modified

--- src/app/componentes/navbar/navbar.component.ts
+++ src/app/componentes/navbar/navbar.component.ts
@@ -9,6 +9,17 @@ export class NavbarComponent implements OnInit {
 
   constructor() { }
 
+  url = window.location.href;
+
+  direction(): boolean{
+    let index;
+    if(this.url === "http://localhost:4200/"){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
   empresa = {id: 1, denominacion: 'Denominacion Empresa', telefono: '123456789', horarioDeAtencion: '6am-4pm PST M-Th, 6am-3pm PST Fri'};
   ngOnInit(): void {
   }
