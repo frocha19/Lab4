735c5a9b1900e8087146c530a8c16680eed1f827
joelvalenti <valentijoel18@gmail.com>
2020-03-28 02:43:10 -0300

    backend done

---
+++ README.md
@@ -0,0 +1,19 @@
+# BackendGenerico
+Backend Generico hecho en Spring 4, una estructura generica aplicada a un simple abm.
+# Tecnologias
+* Java 13
+* Spring 4
+* JPA
+* Hibernate
+* Maven
+* MySQL
+# Dependencias
+* JDBC API
+* MySQL Driver
+* Rest Repositories
+* Rest Repositories HAL Browser
+* Spring Data JPA
+* Spring REST Docs
+* Spring Web
+* Spring Web Services
+* JSon
---
+++ src/main/java/com/utn/demo/services/IservicioGenerico.java
@@ -0,0 +1,17 @@
+package com.utn.demo.services;
+import java.util.List;
+
+public interface IservicioGenerico <E>{
+	
+	public E findById(int id) throws Exception;
+	
+	public E save(E entityForm) throws Exception;
+	
+	public E update (int id, E entityForm) throws Exception;
+	
+	public List<E> findAll (int page, int size) throws Exception;
+	
+	public boolean delete (int id) throws Exception;
+	
+	public int countPages(int size) throws Exception;
+}
